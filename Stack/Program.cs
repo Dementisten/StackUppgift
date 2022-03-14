a849ed9601450f16d68a51574a6f8ba6f717c0a8
Dementisten <69969999+Dementisten@users.noreply.github.com>
2022-03-14 14:07:59 +0100

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            string test = "[]{)}(";
+
+            foreach(char c in test){
+                stack.Push(c);
+            }
+
+            int i = 0;
+            while (stack.Count != 0){
+                
+                char k = stack.Pop();
+                if (k == '('){
+                    i++;
+                }
+                else if (k == ')'){
+                    i--;
+                }
+                else if (k == '{'){
+                    i++;
+                }
+                else if (k == '}'){
+                    i--;
+                }
+                else if (k == '['){
+                    i++;
+                }
+                else if (k == ']'){
+                    i--;
+                }
+            }
+
+            if (i == 0){
+                Console.WriteLine("Balanserad");
+            }
+            else{
+                Console.WriteLine("Inte Balanserad");
+            }
+
+        }
+    }
+}
